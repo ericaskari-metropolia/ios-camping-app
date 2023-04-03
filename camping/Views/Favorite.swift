1a208684aa69a3987b99fbef3d8c1d9ffa80c234
Chi Nguyen <chi.nguyen@live.com>
2023-04-03 21:15:32 +0300

    [features/discovery_page] create bottom navigation

---
+++ camping/Views/Favorite.swift
@@ -0,0 +1,23 @@
+//
+//  Favorite.swift
+//  camping
+//
+//  Created by Chi Nguyen on 3.4.2023.
+//
+
+import SwiftUI
+
+struct Favorite: View {
+    var body: some View {
+        ZStack{
+            Color.blue
+            Text("Welcome to favorite!")
+        }
+    }
+}
+
+struct Favorite_Previews: PreviewProvider {
+    static var previews: some View {
+        Favorite()
+    }
+}
