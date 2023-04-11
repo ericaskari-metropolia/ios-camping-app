f58d4f54272be489be86cddebdaf0ddf32be3d32
Binod Panta <binodpanta@Binods-MBP.local>
2023-04-11 11:14:04 +0300

    Empty trip and trip card added

---
+++ camping/MyTripCards/OngoingTripCard.swift
@@ -0,0 +1,49 @@
+//
+//  OngoingTripCard.swift
+//  camping
+//
+//  Created by Binod Panta on 11.4.2023.
+//
+
+/* This is a card view for ongoing trips  */
+
+//Logic not implemented yet, Mostly hardcoded values.
+
+import SwiftUI
+
+struct OngoingTripCard: View {
+    var body: some View {
+        VStack(alignment: .leading,spacing: 10){
+            ZStack(alignment:Alignment(horizontal: .trailing, vertical: .top)){
+                Image("promo6")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 350,height: 200)
+                    .cornerRadius(15)
+                Image(systemName: "multiply.circle")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))                    .padding()
+            }
+            VStack(alignment: .leading) {
+                    Text("Kotka 2023")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text("16 April 2023 - 28 April 2023")
+                        .foregroundColor(.blue)
+                }
+        }
+        .padding()
+        .background(Rectangle()
+            .foregroundColor(.white)
+            .cornerRadius(20)
+            .shadow(radius: 15))
+        .padding()
+    }
+}
+
+struct OngoingTripCard_Previews: PreviewProvider {
+    static var previews: some View {
+        OngoingTripCard()
+    }
+}
---
+++ camping/Views/MyTripsEmptyView.swift
@@ -0,0 +1,76 @@
+//
+//  MyTripsEmptyView.swift
+//  camping
+//
+//  Created by Binod Panta on 10.4.2023.
+//
+
+/* This view is displayed when the user doesnot have any trip plan yet.*/
+
+
+import SwiftUI
+
+struct MyTripsEmptyView: View {
+    var body: some View {
+        VStack {
+            ZStack(alignment:Alignment(horizontal: .leading, vertical: .bottom)){
+               
+                Image("header")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: 400, maxHeight: 350)
+                    .clipped()
+               
+                VStack{
+                    //The location Espoo is hardcoded, need to be changed
+                    Label("You are in Espoo", systemImage: "globe.europe.africa")
+                      .font(.headline)
+                         .foregroundColor(.white)
+                         .padding(.bottom,5)
+                        
+                         
+                      Text("My trips")
+                          .font(.title)
+                          .foregroundColor(.white)
+                          .fontWeight(.bold)
+                       
+                }
+                .padding()
+                .padding(.bottom,30)
+                }
+           
+          .ignoresSafeArea()
+            Spacer()
+            VStack{
+                Text("Ongoing trips")
+                    .padding(.top,-55)
+                    .padding(.leading, -180)
+                    .fontWeight(.black)
+                
+                //Just a random image, I can change it later
+                Image("no-plan")
+                    .resizable()
+                    .frame(maxWidth: 400, maxHeight: 250)
+                    .padding(.top,-20)
+                    .padding(.horizontal,20)
+                Text("Oops!! You don't have any plans yet!!")
+                    .padding(.bottom,20)
+                Label("Create new trip", systemImage: "plus")
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                
+            }
+        }
+    }
+}
+
+struct MyTripsEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTripsEmptyView()
+    }
+}
