99a29e245769255c65f30d431bed98548831aaef
ericaskari <ericaskari@gmail.com>
2023-04-19 16:18:02 +0300

    Working Snapshot map

--- camping/Views/AddPlan/AddPlanOverview.swift
+++ camping/Views/AddPlan/AddPlanOverview.swift
@@ -5,6 +5,7 @@
 //  Created by Eric Askari on 14.4.2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct AddPlanOverview: View {
@@ -13,26 +14,129 @@ struct AddPlanOverview: View {
     @EnvironmentObject var viewModel: PlanViewModel
 
     @State private var saved = false
+    var span: CLLocationDegrees = 0.01
+
+    @State private var snapshotImage: UIImage? = nil
+
     var completed: () -> ()
 
     var input: AddPlantFirstStepViewOutput?
     var body: some View {
-        VStack {
-            Text(input?.startDate.description ?? "No Start date")
-            Text(input?.endDate.description ?? "No End date")
-            Text(input?.startLocation.latitude.description ?? "No Start latitude")
-            Text(input?.startLocation.longitude.description ?? "No Start latitude")
-            Text(input?.destinationLocation.name ?? "No Destination name")
+        ScrollView {
+            VStack(alignment: .leading) {
+                ZStack {
+                    GeometryReader { geometry in
+                        Group {
+                            if let image = snapshotImage {
+                                Image(uiImage: image)
+                                    .frame(height: 300)
+                            } else {
+                                VStack {
+                                    Spacer()
+                                    HStack {
+                                        Spacer()
+                                        ProgressView().progressViewStyle(CircularProgressViewStyle())
+                                        Spacer()
+                                    }
+                                    Spacer()
+                                }
+                                .background(Color(UIColor.secondarySystemBackground))
+                            }
+                        }
+                        .onAppear {
+                            self.generateSnapshot(width: geometry.size.width, height: geometry.size.height)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 300)
+                .background(Color.red)
 
-            Button {
-                viewModel.savePlan(input: input!)
-                saved = true
-                completed()
-                dismiss()
-            } label: {
-                Text("Save")
+                
+                Text(self.input?.startDate.description ?? "No Start date")
+                Text(self.input?.endDate.description ?? "No End date")
+                Text(self.input?.startLocation.latitude.description ?? "No Start latitude")
+                Text(self.input?.startLocation.longitude.description ?? "No Start latitude")
+                Text(self.input?.destinationLocation.name ?? "No Destination name")
+
+                Button {
+                    self.viewModel.savePlan(input: self.input!)
+                    self.saved = true
+                    self.completed()
+                    self.dismiss()
+                } label: {
+                    Text("Save")
+                }
+                .disabled(self.input == nil || self.saved)
             }
-            .disabled(input == nil || saved)
+            .background(Color.red)
+        }.ignoresSafeArea()
+    }
+
+    func generateImageUrl(start: CLLocationCoordinate2D, end: CLLocationCoordinate2D) {}
+
+    func generateSnapshot(width: CGFloat, height: CGFloat) {
+        let coordinate1 = self.input?.startLocation ?? CLLocationCoordinate2D(latitude: 60.192059, longitude: 24.945831)
+
+        let coordinate2 = CLLocationCoordinate2D(latitude: self.input?.destinationLocation.latitude ?? 60.229192, longitude: self.input?.destinationLocation.longitude ?? 24.748633)
+
+        // convert them to MKMapPoint
+        let p1 = MKMapPoint(coordinate1)
+        let p2 = MKMapPoint(coordinate2)
+
+        let rectPadding = 70000
+
+        let mapRect = MKMapRect(
+            x: fmin(p1.x, p2.x) - Double(rectPadding) / 2,
+            y: fmin(p1.y, p2.y) - Double(rectPadding) / 2,
+            width: fabs(p1.x - p2.x) + Double(rectPadding),
+            height: fabs(p1.y - p2.y) + Double(rectPadding)
+        )
+
+        // Map options.
+        let mapOptions = MKMapSnapshotter.Options()
+        mapOptions.mapRect = mapRect
+        mapOptions.size = CGSize(width: width, height: height)
+
+        // Create the snapshotter and run it.
+        let snapshotter = MKMapSnapshotter(options: mapOptions)
+
+        snapshotter.start { snapshotOrNil, errorOrNil in
+            if let error = errorOrNil {
+                print(error)
+                return
+            }
+
+            if let snapshot = snapshotOrNil {
+                let mapImage = snapshot.image
+
+                let point1 = snapshot.point(for: coordinate1)
+                let point2 = snapshot.point(for: coordinate2)
+
+                let finalImage = UIGraphicsImageRenderer(size: mapOptions.size).image { _ in
+                    UIColor.white.setFill()
+
+                    let configuration = UIImage.SymbolConfiguration(pointSize: 20)
+
+                    let pinImage = UIImage(
+                        systemName: "location.north.circle.fill",
+                        withConfiguration: configuration
+                    )!
+                    let pinImage2 = UIImage(
+                        systemName: "tent.circle.fill",
+                        withConfiguration: configuration
+                    )!
+
+                    let pinImageCGPoint1 = CGPoint(x: point1.x - pinImage.size.width / 2, y: point1.y - pinImage.size.height / 2)
+
+                    let pinImageCGPoint2 = CGPoint(x: point2.x - pinImage2.size.width / 2, y: point2.y - pinImage2.size.height / 2)
+
+                    mapImage.draw(at: .zero)
+                    pinImage.draw(at: pinImageCGPoint1)
+                    pinImage2.draw(at: pinImageCGPoint2)
+                }
+                self.snapshotImage = finalImage
+            }
         }
     }
 }
