0f8f346538fda91ea4e42d0b94930aeedf147ad0
ericaskari <ericaskari@gmail.com>
2023-04-20 22:57:41 +0300

    GearList and Preview State

---
+++ camping/ViewModel/GearViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  AddPlanViewModel.swift
+//  camping
+//
+//  Created by Thu Hoang on 10.4.2023.
+//
+import CoreData
+import Foundation
+import MapKit
+import SwiftUI
+
+ class GearViewModel: ObservableObject {
+    var context: NSManagedObjectContext
+
+//    @Environment(\.managedObjectContext) private var context
+    
+    //  Saves Plan to Storage
+    func save(name: String, plan: Plan, checked: Bool = false) {
+        let context = PersistenceController.shared.container.viewContext
+
+        let gear = Gear(context: context)
+        gear.id = UUID()
+        gear.name = name
+        gear.plan = plan
+        gear.checked = checked
+
+        do {
+            try context.save()
+            print("Gear Saved!")
+        } catch {
+            print("Cannot save Gear.")
+        }
+    }
+
+    func deleteItems(offsets: IndexSet, items: FetchedResults<Gear>) {
+        let context = PersistenceController.shared.container.viewContext
+        withAnimation {
+            offsets.map { items[$0] }.forEach(context.delete)
+
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    func addItem() {
+//        let context = PersistenceController.shared.container.viewContext
+        withAnimation {
+            let newItem = Gear(context: context)
+            newItem.name = Date().description
+            newItem.id = UUID()
+            newItem.checked = false
+//            newItem.plan
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
---
+++ camping/Views/Gears/GearListView.swift
@@ -0,0 +1,69 @@
+//
+//  GearListView.swift
+//  camping
+//
+//  Created by Eric Askari on 19.4.2023.
+//
+
+import CoreData
+import SwiftUI
+
+struct GearListView: View {
+    var gearViewModel: GearViewModel = .init()
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Gear.name, ascending: true)],
+        animation: .default
+    )
+    private var items: FetchedResults<Gear>
+
+    @ObservedObject private var viewModel: GearViewModel
+
+       init(context: NSManagedObjectContext) {
+           self.viewModel = GearViewModel(context: context)   // initialize
+       }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { _ in
+                    NavigationLink {
+                        Text("Item at ")
+                    } label: {
+                        Text("Label")
+                    }
+                }
+                .onDelete(perform: {
+                    indexSet in
+                    gearViewModel.deleteItems(offsets: indexSet, items: items)
+                })
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: gearViewModel.addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            Text("Select an item")
+        }
+    }
+}
+
+struct GearListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GearListView().environmentObject(
+            GearViewModel()
+        ).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
