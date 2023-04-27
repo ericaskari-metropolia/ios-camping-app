410cc36dfc3dcbc08bf4cdd3ba2c26ec957ce5d5
Chi Nguyen <chi.nguyen@live.com>
2023-04-27 21:45:01 +0300

    fix navigation bar on campsite detail view

--- camping/Views/HomeViews/CampsiteDetail/CampsiteDetailView.swift
+++ camping/Views/HomeViews/CampsiteDetail/CampsiteDetailView.swift
@@ -2,12 +2,14 @@
 //  CampsiteDetailView.swift
 //  camping
 //
-//  Created by Chi Nguyen on 14.4.2023.
+//  Created by The Minions on 14.4.2023.
 //
+
 
 import SwiftUI
 import MapKit
 
+// MARK: Display all information related to chosen campsite
 struct CampsiteDetailView: View {
     //PROPERTIES
     @Environment(\.dismiss) var dismiss
@@ -20,16 +22,52 @@ struct CampsiteDetailView: View {
     @State var weather: ResponseBody?
     
     var body: some View {
+        NavigationView {
             VStack(){
                 // Campsite Image
-                AsyncImage(url: URL(string: (campsite.imageURL) ?? "header")) { image in
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .frame(maxWidth: UIScreen.main.bounds.width)
-                        .frame(height: 300)
-                } placeholder: {
-                    ProgressView()
+                ZStack(alignment: .top){
+                    
+                    AsyncImage(url: URL(string: (campsite.imageURL) ?? "header")) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: UIScreen.main.bounds.width)
+                            .frame(height: 300)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    VStack(spacing: 0.0){
+                        HStack {
+                            
+                            // Button to go back previous page
+                            Button{
+                                dismiss()
+                            } label: {
+                                Image(systemName: "chevron.left")
+                                    .font(.title)
+                                    .padding()
+                                
+                            }
+                            .symbolVariant(.circle.fill)
+                            .foregroundStyle(Color("PrimaryColor"), .white)
+                            
+                            Spacer()
+                            
+                            // Button to add or remove campsite from favorite
+                            Button{
+                                campsite.isFavorite.toggle()
+                            } label: {
+                                Image(systemName: "heart.square")
+                                        .font(.title)
+                                        .fontWeight(.heavy)
+                                        .foregroundColor(campsite.isFavorite ? .red : .white)
+                                        .background(Color("PrimaryColor"))
+                                        .padding()
+                            }
+                        }
+                        .padding(EdgeInsets(top: 50, leading: 20, bottom: 0, trailing: 20))
+                    }
                 }
                 .background(Color.clear)
                 
@@ -119,6 +157,7 @@ struct CampsiteDetailView: View {
                         })
                         
                         
+                        // Link to map view and add plan
                         HStack{
                             
                             NavigationLink{
@@ -128,15 +167,16 @@ struct CampsiteDetailView: View {
                             }
                             
                             PlanNewTripButtonView(campsite: campsite) {
-                                Text("Plan new trip")
-                                    .font(.system(.title3, design: .rounded))
-                                    .foregroundColor(.white)
-                            } completed: {
-                                // Nothing to do
-                            }
-                            .padding(15)
-                            .background(Color.primary)
-                            .clipShape(Capsule())
+                                                        Text("Plan new trip")
+                                                            .font(.system(.title3, design: .rounded))
+                                                            .foregroundColor(.white)
+                                                    } completed: {
+                                                        // Nothing to do
+                                                    }
+                                                    .padding(15)
+                                                    .background(Color.primary)
+                                                    .clipShape(Capsule())
+                            
                             
                         }
                         .padding()
@@ -146,17 +186,187 @@ struct CampsiteDetailView: View {
                 
             }
             .edgesIgnoringSafeArea(.top)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        campsite.isFavorite.toggle()
-                    } label: {
-                        Image(systemName: campsite.isFavorite ? "heart.fill" : "heart")
-                            .foregroundColor(campsite.isFavorite ? .red : .black)
-                    }
-                }
-            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
     
 }
+
+
+
+//struct CampsiteDetailView: View {
+//    //PROPERTIES
+//    @Environment(\.dismiss) var dismiss
+//    @Environment(\.managedObjectContext) private var moc
+//    let provider = PersistenceController.shared
+//
+//    @ObservedObject var campsite: CampingSite
+//    var weatherManager = WeatherManager()
+//    @State var weather: ResponseBody?
+//
+//    var body: some View {
+//        VStack(){
+//            // Campsite Image
+//                            ZStack(alignment: .top){
+//
+//                                AsyncImage(url: URL(string: (campsite.imageURL) ?? "header")) { image in
+//                                    image
+//                                        .resizable()
+//                                        .scaledToFill()
+//                                        .frame(maxWidth: UIScreen.main.bounds.width)
+//                                        .frame(height: 300)
+//                                } placeholder: {
+//                                    ProgressView()
+//                                }
+//                                .edgesIgnoringSafeArea(.top)
+//
+//                                VStack(spacing: 0.0){
+//                                    HStack {
+//
+//                                        // Button to go back previous page
+//                                        Button{
+//                                            dismiss()
+//                                        } label: {
+//                                            Image(systemName: "chevron.left.circle.fill")
+//                                                .font(.title)
+//                                                .foregroundColor(.white)
+//                                                .padding()
+//                                        }
+//
+//                                        Spacer()
+//
+//                                        // Button to add or remove campsite from favorite
+//                                        Button{
+//                                            campsite.isFavorite.toggle()
+//                                        } label: {
+//                                            Image(systemName: "heart.fill")
+//                                                .font(.title)
+//                                                .foregroundColor(campsite.isFavorite ? .red : .white)
+//                                                .padding()
+//
+//                                        }
+//                                    }
+//                                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
+//                                }
+//                            }
+//                            .background(Color(.clear))
+//
+//            // Campsite detail
+//            ScrollView {
+//                VStack(alignment: .leading, spacing: 20) {
+//
+//                    Text(campsite.name ?? "Not available")
+//                        .font(.title)
+//                        .multilineTextAlignment(.center)
+//                        .foregroundColor(.primary)
+//
+//                    HStack{
+//                        if let weather = weather {
+//                            WeatherConditionView(weather: weather)
+//                        }else {
+//                            ProgressView()
+//                                .task{
+//                                    do{
+//                                        weather = try await weatherManager.getCurrentWeather(latitude: campsite.latitude, longtitude: campsite.longitude)
+//                                    }catch{
+//                                        print("Error getting weather condition: \(error)")
+//                                    }
+//                                }
+//                        }
+//                    }
+//                    .padding()
+//                    HStack {
+//
+//                        Spacer()
+//
+//                        VStack(alignment: .center, spacing: 5){
+//                            Text("Campfire area")
+//                                .font(.headline)
+//                            if campsite.hasCampfireSite == true {
+//                                Text("Available")
+//                                    .font(.subheadline)
+//                            }else {
+//                                Text("Not available")
+//                                    .font(.subheadline)
+//                            }
+//                        }
+//
+//                        Spacer()
+//                        VStack(alignment: .center, spacing: 5){
+//                            Text("Rental Hut")
+//                                .font(.headline)
+//                            if campsite.hasRentalHut == true {
+//                                Text("Available")
+//                                    .font(.subheadline)
+//                            }else {
+//                                Text("Not Available")
+//                                    .font(.subheadline)
+//                            }
+//                        }
+//
+//                        Spacer()
+//                    }
+//
+//                    Text("Description")
+//                        .font(.headline)
+//                        .padding(.horizontal)
+//                    Text(campsite.descriptionEN ?? "nil")
+//                        .font(.subheadline)
+//                        .multilineTextAlignment(.leading)
+//                        .foregroundColor(.primary)
+//                        .padding(.horizontal)
+//                    Text("Accessibilty")
+//                        .font(.headline)
+//                        .padding(.horizontal)
+//                    Text(campsite.suitabilityEN ?? "")
+//                        .font(.subheadline)
+//                        .multilineTextAlignment(.leading)
+//                        .foregroundColor(Color.primary)
+//                        .padding(.horizontal)
+//
+//                    Text("Website")
+//                        .font(.headline)
+//                        .padding(.horizontal)
+//                    Link(destination: URL(string: "\(campsite.websiteURL ?? "")")!, label: {
+//                        Text(campsite.websiteURL ?? "")
+//                            .font(.subheadline)
+//                            .multilineTextAlignment(.leading)
+//                            .foregroundColor(Color(.systemBlue))
+//                            .padding(.horizontal)
+//                    })
+//
+//
+//                    HStack{
+//
+//                        NavigationLink{
+//                            CampsiteMapView(campsite: campsite)
+//                        } label: {
+//                            ViewOnMapButtonView()
+//                        }
+//
+//                        PlanNewTripButtonView(campsite: campsite) {
+//                            Text("Plan new trip")
+//                                .font(.system(.title3, design: .rounded))
+//                                .foregroundColor(.white)
+//                        } completed: {
+//                            // Nothing to do
+//                        }
+//                        .padding(15)
+//                        .background(Color.primary)
+//                        .clipShape(Capsule())
+//
+//                    }
+//                    .padding()
+//                }
+//                .padding()
+//            }
+//            .edgesIgnoringSafeArea(.top)
+//
+//        }
+//        .navigationBarBackButtonHidden(true)
+//    }
+//
+//}
+
+    
 
